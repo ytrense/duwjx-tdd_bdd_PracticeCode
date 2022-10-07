66d45b626040d8ed9998af2f0ba54d56d64fa84b
ytrense <yves@mailbox.org>
2022-10-07 06:16:56 -0400

    did the labs 05

--- labs/05_factories_and_fakes/tests/factories.py
+++ labs/05_factories_and_fakes/tests/factories.py
@@ -20,3 +20,9 @@ class AccountFactory(factory.Factory):
         model = Account
 
     # Add attributes here...
+    id = factory.Sequence(lambda n: n)
+    name = factory.Faker("name")
+    email = factory.Faker("email")
+    phone_number = factory.Faker("phone_number")
+    disabled = FuzzyChoice(choices=[True, False])
+    date_joined = FuzzyDate(date(2008, 1, 1))
